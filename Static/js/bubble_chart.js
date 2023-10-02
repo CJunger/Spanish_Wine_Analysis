53d4d60356858eeb441a3ec2b0cd481febc13b24
Morgan Missel <morgan.missel@gmail.com>
2023-10-02 16:24:44 -0500

    Updating bubble chart

--- Static/js/bubble_chart.js
+++ Static/js/bubble_chart.js
@@ -1,69 +1,74 @@
 // Price vs Rating
 
-function init() {
-    buildBubble();
-};
+d3.json('spanish_wine.json').then((dataObject) => {
+    // Converting data type
+    const data = Object.values(dataObject);
+    
+    // Extract unique wineries from data
+    const uniqueWineries = [...new Set(data.map(row => row.winery))];
+    
+    // Populate dropwdown menu with winery options
+    const dropdown = document.getElementById('winery-dropdown');
+    uniqueWineries.forEach(winery =>{
+        const option = document.createElement('option');
+        option.value = winery;
+        option.text = winery;
+        dropdown.appendChild(option);
+    });
 
-init();
+    buildBubble(data);
 
-function buildBubble() {
-    d3.json('spanish_wine.json').then((data) => {
+    // Adding event listener
+    dropdown.addEventListener('change', function (){
+        const selectedWinery = this.value; // Get the winery
 
-        // console.log(data);
+        //Filter data
+        const filteredData = Object.values(data).filter(row => row.winery === selectedWinery);
 
-        let winery = []
-        let wine = []
-        // let year = []
-        let rating = []
-        // let num_reviews = []
-        // let region = []
-        let price = []
-        // let type = []
-        // let idCount
-        // let id = [] 
+        buildBubble(filteredData)
+    });
+});
 
-        for (key in data) {
-            row=data[key];
-            // console.log(row);
-            price.push(row.price);
-            rating.push(row.rating);
-            wine.push(row.wine);
-            winery.push(row.winery);
-            console.log(price, rating, wine, winery);
-        };
+// Creating function to build bubble charts
+function buildBubble(data) {
+    const price = data.map(row => row.price)
+    const rating = data.map(row => row.rating)
+    const wine = data.map(row => row.wine)
 
-        // console.log(id);
+    let trace1 = {
+        x: price,
+        y: rating,
+        text: wine,
+        mode: "markers",
+        name: "Price of Bottle",
+        type: "bubble",
+        marker: {size: price.map(p => p*0.25)
+        }
+    };
 
-        let trace1 = {
-            x: price,
-            y: rating,
-            text: wine,
-            mode: "markers",
-            name: "Price of Bottle",
-            type: "bubble",
-            marker: {size: price.map(p => p*0.1)
-            }
-        };
+    let plot = [trace1];
 
-        let plot = [trace1];
+    let layout = {
+        title: "Price of Bottle Compared to Rating of Wine",
+        margin: {automargin:true},
+        hovermode: "closest",
+        xaxis: {
+            title: 'Price',
+            ticktext: [price],
+            tickvals: price,
+        },
+        yaxis: {
+            title: 'Rating',
+            ticktext: [rating],
+            tickvals: rating,
+        }
+    };
 
-        let layout = {
-            title: "Price of Bottle Compared to Rating of Wine",
-            margin: {automargin:true},
-            hovermode: "closest",
-            xaxis: {
-                title: 'Price',
-                ticktext: [price],
-                tickvals: price,
-            },
-            yaxis: {
-                title: 'Rating',
-                ticktext: [rating],
-                tickvals: rating,
-            }
-        };
+    if (window.myplot) {
+        Plotly.react('price-rating-plot', plot, layout);
+    } else {
+        window.myPlot = Plotly.newPlot("price-rating-plot", plot, layout);
+    }
+}; 
 
-        Plotly.newPlot("price-rating-plot", plot, layout);
-    }); 
-};
 
